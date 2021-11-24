313c521369cc5e27ab369e4b140f8e19a14f4019
ThiagoA20 <thiagohca2019@gmail.com>
2021-11-24 16:21:05 -0600

    firebase crud

--- backend/server.py
+++ backend/server.py
@@ -13,9 +13,13 @@ firebaseConfig = {
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 
-# db = firebase.database()
+db = firebase.database()
 # auth = firebase.auth()
-storage = firebase.storage()
+# storage = firebase.storage()
+
+
+
+
 
 # Autentication
 # Login
@@ -36,6 +40,9 @@ storage = firebase.storage()
     # print("User successfully created!")
 
 
+
+
+
 #Storage
 #upload
 # filename = input("Type the name of the file that you want to upload: ")
@@ -43,5 +50,40 @@ storage = firebase.storage()
 # storage.child(cloudFilename).put(filename)
 
 #download
-cloudFilename = input("Type the name of the file on the cloud: ")
-storage.child(cloudFilename).download("", "downloaded.txt")
+# cloudFilename = input("Type the name of the file on the cloud: ")
+# storage.child(cloudFilename).download("", "downloaded.txt")
+
+
+
+
+
+# Database
+
+# Create
+data = {'age': 40, 'employed': True, 'name': "Thiago", 'address': 'Montreal'}
+# db.child("people").child(data['name']).set(data)
+# db.child("people").push(data)
+# db.push(data)
+
+# Update
+# db.child("people").child(data['name']).update({"age": 32})
+
+# add property:
+# db.child("people").child(data['name']).update({"time": 23})
+
+# add to a specific person:
+# people = db.child("people").get()
+# for people in people.each():
+    # if people.key() == "Thiago":
+        # db.child("people").child("Thiago").update({"age": 57})
+    # else:
+        # db.child("people").child(people.key()).update({"age": 82})
+
+# Delete
+# db.child("people").child("Thiago").remove()
+
+people = db.child("people").get()
+
+for people in people.each():
+    if people.val()['name'] == "Thiago":
+        db.child("people").child(people.key()).child("age").remove()

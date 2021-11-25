81b909602e83f99438a13409f991b4991f9ec8ad
ThiagoA20 <thiagohca2019@gmail.com>
2021-11-25 17:09:53 -0600

    blitzkrieg

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,8 @@
 import pyrebase
+# from http.server import BaseHTTPRequestHandler, HTTPServer
+# import json
+# import time
+
 
 firebaseConfig = {
     'apiKey': "AIzaSyBkqUE0-byq4QduF-ENQk_TkG4md4u-dc0",
@@ -60,7 +64,7 @@ db = firebase.database()
 # Database
 
 # Create
-data = {'age': 40, 'employed': True, 'name': "Thiago", 'address': 'Montreal'}
+# data = {'age': 40, 'employed': True, 'name': "Thiago", 'address': 'Montreal'}
 # db.child("people").child(data['name']).set(data)
 # db.child("people").push(data)
 # db.push(data)
@@ -89,3 +93,68 @@ data = {'age': 40, 'employed': True, 'name': "Thiago", 'address': 'Montreal'}
         # db.child("people").child(people.key()).child("age").remove()
 
 # Read
+
+
+# hostName = "0.0.0.0"
+# serverPort = 5000
+
+# class Server(BaseHTTPRequestHandler):
+
+#     def do_OPTIONS(self):
+#         pass       
+#         # self.send_response(200, "ok")       
+#         # self.send_header('Access-Control-Allow-Origin', '*')                
+#         # self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+#         # self.send_header("Access-Control-Allow-Headers", "X-Requested-With")        
+#         # self.end_headers()
+
+#     def do_POST(self):
+#         self.send_response(200)
+#         self.send_header('Content-type', 'application/json')
+#         self.send_header('Access-Control-Allow-Origin', '*')
+#         self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+#         self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+#         self.send_header("Access-Control-Allow-Headers", "Content-Type")
+#         self.end_headers()
+#         content_len = int(self.headers.get('content-length', 0))
+#         post_body = json.loads(self.rfile.read(content_len))
+#         db.child("numbers").push({'number': post_body['number']})
+#         #     molinput = MoleculeGenerator(post_body['molecule'])
+#         #     self.wfile.write(json.dumps(molinput).encode())
+
+#     def do_GET(self):
+#         pass
+#         # test_formula = atomlist
+#         # if self.path == '/periodictable':
+#         #     self.send_response(200)
+#         #     self.send_header('Content-Type', 'application/json')
+#         #     self.send_header('Access-Control-Allow-Origin', '*')
+#         #     self.end_headers()
+#         #     self.wfile.write(json.dumps(test_formula).encode())
+
+# if __name__ == "__main__":        
+#     webServer = HTTPServer((hostName, serverPort), Server)
+#     print("Server started http://%s:%s" % (hostName, serverPort))
+
+#     try:
+#         webServer.serve_forever()
+#     except KeyboardInterrupt:
+#         pass
+
+#     webServer.server_close()
+#     print("Server stopped.")
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
